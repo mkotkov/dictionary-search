22da5c2b2e7ea3d20514e7f97a3fc6f5c8bc4e83
mkotkov <maximkotkov@gmail.com>
2025-12-22 17:44:20 +0200

    21.12.2025: word fech from Ekilex

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.core.config import origins
+from core.config import origins
+from services.ekilex import fetch_ekilex_word, fetch_ekilex_word_from_dataset
 
 app = FastAPI()
 
@@ -14,5 +15,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/ekilex/{word}")
+async def get_ekilex_word(word: str):
+
+    data = fetch_ekilex_word(word)
+    if data is None:
+        return {"message": "Word not found"}
+
+    return data
+
+@app.get("/ekilex/{word}/{dataset}")
+async def get_ekilex_word_from_dataset(word: str, dataset: str):
+
+    data = fetch_ekilex_word_from_dataset(dataset, word)
+    if data is None:
+        return {"message": "Word not found"}
+
+    return data
+
+
 if __name__ == "__main__":
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/services/ekilex.py
+++ backend/services/ekilex.py
@@ -1,11 +1,10 @@
-import requests
 import json
-from fastapi import FastAPI
+import requests
 
 from core.config import API_KEY_EKILEX # API-key Ekilex
 
-
-def ekilex_word_fetch(word):
+# Fetch word data from Ekilex
+def fetch_ekilex_word(word):
     try:
         # Sent request to Ekilex API for word searching
         response = requests.get(
@@ -21,11 +20,58 @@ def ekilex_word_fetch(word):
 
         # Parse JSON response
         data = response.json()
-
+    
         # Check if there are results
         if not data:
             print('Word not found')
             return
+
+        results = data.get('words', [])
+
+        filtered_words = []
+        for words in results:
+                filtered_words.append({
+                    'wordId': words.get('wordId'),
+                    'wordValue': words.get('wordValue'),
+                    'datasetCodes': words.get('datasetCodes', []),
+                })
+
+        # Return filtered word data
+        return filtered_words
     
     except Exception as e:
         print('Error:', str(e))
+
+
+# Fetch word data from specific dataset
+def fetch_ekilex_word_from_dataset(dataset, word):
+    try:
+        response = requests.get(
+            f'https://ekilex.ee/api/meaning/search/{word}/{dataset}',
+            headers={
+                'ekilex-api-key': API_KEY_EKILEX
+            }
+        )
+
+        if response.status_code != 200:
+            raise Exception(f"Request error: {response.status_code} {response.reason}")
+
+        data = response.json()
+        results = data.get('results', [])
+
+        filtered_words = []
+
+        for meaning in results:
+            for w in meaning.get('meaningWords', []):
+                filtered_words.append({
+                    'wordId': w.get('wordId'),
+                    'wordValue': w.get('wordValue'),
+                    'lang': w.get('lang'),
+                    'datasetCodes': w.get('datasetCodes', []),
+                })
+
+        return filtered_words
+
+    except Exception as e:
+        print(f'Error with {dataset}:', str(e))
+        return []
